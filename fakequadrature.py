b8d95369b0ff9a1c4bd1cea7847f81cf517be40e
pog87 <pog87@hotmail.it>
2019-12-17 14:48:33 +0100

    Added NBs

--- fakequadrature.py
+++ fakequadrature.py
@@ -2,23 +2,21 @@ import numpy as np
 import numpy.matlib
 from scipy.integrate import quad
 
-def lebesgue(x, xx):
-    L = np.abs( lagrange(x,xx) )
-    return np.sum( L, axis = -1 )
 
 def lagrange_k(x, nodes, k):
-    nodes_ = list(np.delete(nodes,k))
+    nodes_ = np.delete(nodes,k)
     #l_k=1
     #for nj in nodes_:
     #    l_k *= (x-nj)/(nodes[k]-nj)
-    l_k = np.prod((x-nodes)/(nodes[k]-nodes) )
+    l_k = np.prod((x-nodes_)/(nodes[k]-nodes_) )
     return l_k
 
-def quadrature_weights(nodes, Interval, S = None):
+def quadrature_weights(nodes, interval, mapping = None):
     N_nodes = len(nodes)
-    a,b = Interval
-    if S is None:
-        S=lambda x:x
-    lagrange_functs = lambda x: lagrange_k(S(x), S(nodes),k)
-    weights = [quad( lagrange_functs, a, b)[0] for k in range(N_nodes)]
+    a,b = interval
+    if mapping is None:
+        mapping = lambda x: x       
+    funcf = lambda y,k: lagrange_k(mapping(y),mapping(nodes),k)
+    weights = [ quad( funcf, a, b, args=(k,))[0] for k in range(N_nodes) ]
     return np.array(weights)
+
