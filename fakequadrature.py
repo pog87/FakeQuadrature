d7e72ef68ebf20e2795aa91142c7467ba0392a2d
Emma Perracchione <47738723+emmaA89@users.noreply.github.com>
2020-01-08 11:12:03 +0100

    Update fakequadrature.py

--- fakequadrature.py
+++ fakequadrature.py
@@ -5,9 +5,6 @@ from scipy.integrate import quad
 
 def lagrange_k(x, nodes, k):
     nodes_ = np.delete(nodes,k)
-    #l_k=1
-    #for nj in nodes_:
-    #    l_k *= (x-nj)/(nodes[k]-nj)
     l_k = np.prod((x-nodes_)/(nodes[k]-nodes_) )
     return l_k
 
