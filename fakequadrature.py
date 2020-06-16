cdbb7a1c6a94e8af09ba4967bcc2168a4f2a5b27
pog87 <pog87@email.it>
2020-06-16 13:53:34 +0200

    correzioni Emma

--- fakequadrature.py
+++ fakequadrature.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.matlib
-#from scipy.integrate import quad
+from scipy.integrate import quad
 from scipy.integrate import simps
 
 
@@ -17,8 +17,8 @@ def quadrature_weights(nodes, interval, mapping = None):
     if mapping is None:
         mapping = lambda x: x       
     lagr = lambda y,k: lagrange_k(mapping(y),mapping(nodes),k)
-    #weights = [ quad( lagr, a, b, epsabs = 1.e-9, limit=250, args=(k,))[0] for k in range(N_nodes) ]
-    x = np.linspace( a, b, 10*N_nodes+1 )
-    weights = [ simps(lagr(x,k), x) for k in range(N_nodes) ]
+    weights = [ quad( lagr, a, b, epsabs = 1.e-9, limit=250, args=(k,))[0] for k in range(N_nodes) ]
+    #x = np.linspace( a, b, 10*N_nodes+1 )
+    #weights = [ simps(lagr(x,k), x) for k in range(N_nodes) ]
     return np.array(weights)
 
