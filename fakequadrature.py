bbe1288349eef946f9ceef0c280f2c51e04b883d
pog87 <pog87@email.it>
2020-06-16 09:37:41 +0200

    the cavalieri-simpson way

--- fakequadrature.py
+++ fakequadrature.py
@@ -1,11 +1,14 @@
 import numpy as np
 import numpy.matlib
-from scipy.integrate import quad
+#from scipy.integrate import quad
+from scipy.integrate import simps
 
 
 def lagrange_k(x, nodes, k):
-    nodes_ = np.delete(nodes,k)
-    l_k = np.prod((x-nodes_)/(nodes[k]-nodes_) )
+    nodes_but = np.delete(nodes,k)
+    D = x-nodes_but[:,None]
+    N = nodes[k]-nodes_but
+    l_k = np.prod(D/N[:,None], axis = 0 )
     return l_k
 
 def quadrature_weights(nodes, interval, mapping = None):
@@ -13,7 +16,9 @@ def quadrature_weights(nodes, interval, mapping = None):
     a,b = interval
     if mapping is None:
         mapping = lambda x: x       
-    funcf = lambda y,k: lagrange_k(mapping(y),mapping(nodes),k)
-    weights = [ quad( funcf, a, b, epsabs = 1.e-9, limit=250, args=(k,))[0] for k in range(N_nodes) ]
+    lagr = lambda y,k: lagrange_k(mapping(y),mapping(nodes),k)
+    #weights = [ quad( lagr, a, b, epsabs = 1.e-9, limit=250, args=(k,))[0] for k in range(N_nodes) ]
+    x = np.linspace( a, b, 10*N_nodes+1 )
+    weights = [ simps(lagr(x,k), x) for k in range(N_nodes) ]
     return np.array(weights)
 
